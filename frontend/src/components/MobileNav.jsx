a428cc57c667f125d78629a3d1f2219f4c0c34df
Alexandre Teixeira <alexandreteixeiraac@gmail.com>
2025-04-19 03:01:07 -0300

    Update MobileNav.jsx

--- frontend/src/components/MobileNav.jsx
+++ frontend/src/components/MobileNav.jsx
@@ -13,7 +13,7 @@ const MobileNav = () => {
       path: '/dashboard',
       label: 'Home',
       icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
         </svg>
       )
@@ -22,7 +22,7 @@ const MobileNav = () => {
       path: '/celulares',
       label: 'Celulares',
       icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
         </svg>
       )
@@ -31,7 +31,7 @@ const MobileNav = () => {
       path: '/acessorios',
       label: 'Acessórios',
       icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"></path>
         </svg>
       )
@@ -40,7 +40,7 @@ const MobileNav = () => {
       path: '/planos',
       label: 'Planos',
       icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
         </svg>
       )
@@ -54,17 +54,18 @@ const MobileNav = () => {
           <Link
             key={item.path}
             to={item.path}
-            className={`flex flex-col items-center py-3 px-2 ${
+            className={`flex flex-col items-center py-2 px-1 ${
               isActive(item.path)
                 ? 'text-blue-600'
                 : 'text-gray-500 hover:text-gray-700'
             }`}
           >
-            <div className="w-6 h-6">{item.icon}</div>
-            <span className="text-xs mt-1">{item.label}</span>
+            <div className="w-5 h-5">{item.icon}</div>
+            <span className="text-[10px] mt-1">{item.label}</span>
           </Link>
         ))}
       </div>
+      <div className="h-safe-area-bottom"></div>
     </div>
   );
 };
