6d9d83f9d7a201f48278d668ce7751a9ba8aa7cf
Alexandre Teixeira <alexandreteixeiraac@gmail.com>
2025-04-21 01:28:11 -0300

    Update CelularFormPage.jsx

--- frontend/src/pages/CelularFormPage.jsx
+++ frontend/src/pages/CelularFormPage.jsx
@@ -134,7 +134,6 @@ const CelularFormPage = () => {
               value={celular.imei}
               onChange={handleChange}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
             />
           </div>
         </div>
