448146d1912364cfb0c8c43183b267b2c4b934e8
Alexandre Teixeira <alexandreteixeiraac@gmail.com>
2025-05-06 01:40:25 -0300

    Update dashboardController.js

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -8,26 +8,19 @@ const PlanoMovel = require('../models/PlanoMovel');
 exports.getDashboardStats = async (req, res) => {
     try {
         // Contar documentos em paralelo
-        const [totalCelulares, totalAcessorios, totalPlanos] = await Promise.all([
-            // Aggregate para somar a quantidade de todos os celulares
-            Celular.aggregate([
-                { $group: { _id: null, total: { $sum: '$quantidade' } } }
-            ]),
-            // Aggregate para somar a quantidade de todos os acessórios
-            Acessorio.aggregate([
-                { $group: { _id: null, total: { $sum: '$quantidade' } } }
-            ]),
-            // Contagem simples de planos
+        const [totalCelularesDocs, totalAcessoriosDocs, totalPlanos] = await Promise.all([
+            // Contar o número total de documentos na coleção Celular
+            Celular.countDocuments(),
+            // Contar o número total de documentos na coleção Acessorio
+            Acessorio.countDocuments(),
+            // Contagem simples de planos (mantém a mesma lógica)
             PlanoMovel.countDocuments()
         ]);
 
-        // Extrair os totais dos resultados do aggregate (ou 0 se não houver itens)
-        const quantidadeCelulares = totalCelulares.length > 0 ? totalCelulares[0].total : 0;
-        const quantidadeAcessorios = totalAcessorios.length > 0 ? totalAcessorios[0].total : 0;
-
+        // Os resultados de countDocuments já são os números totais
         res.status(200).json({
-            celulares: quantidadeCelulares,
-            acessorios: quantidadeAcessorios,
+            celulares: totalCelularesDocs,     // Retorna a contagem de documentos
+            acessorios: totalAcessoriosDocs,   // Retorna a contagem de documentos
             planos: totalPlanos
         });
 
